7bf3d5a0563300fe759f8697f9b1b294abeb1e15
Lyn <lisa.s.jeong@gmail.com>
2025-01-31 14:31:34 -0500

    with SimpleAmqpClient

--- producer.cpp
+++ producer.cpp
@@ -1,23 +1,13 @@
-#include <amqpcpp.h>
+#include <SimpleAmqpClient/SimpleAmqpClient.h>
 #include <iostream>
-#include "MyConnectionHandler.h"
 
 int main() {
-    // boost::asio::io_context io_context;
+    auto channel = AmqpClient::Channel::Create("localhost");
 
-    // AMQP::LibBoostAsioHandler handler(io_context);
-    MyConnectionHandler handler;
-    
-    AMQP::Connection connection(&handler, AMQP::Address("amqp://guest:guest@localhost:15672"));
-    AMQP::Channel channel(&connection);
-    // AMQP::TcpConnection connection(&handler, );
-    // AMQP::TcpChannel channel(&connection);
+    channel->DeclareExchange("exchange_lyn", AmqpClient::Channel::EXCHANGE_TYPE_DIRECT);
 
-    channel.declareExchange("exchange_lyn", AMQP::direct);
+    std::string message = "Hello World~!";
+    channel->BasicPublish("exchange_lyn", "routing_key_lyn", AmqpClient::BasicMessage::Create(message));
 
-    std::string message = "Hello World!";
-    std::string routing_key = "key_lyn";
-
-    channel.publish("exchange_lyn", routing_key, message);
     std::cout << "Message sent: " << message << std::endl;
 }
